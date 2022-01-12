baec1dc22693a27070709ef40fa4bfa2c5d03147
Alessandro Alberga <ale.alberga1@gmail.com>
2022-01-12 23:06:37 +1100

    Core chaper 8 complete

---
+++ Chapter8/C#/question-8.3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace C_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine($"Magic index is: {FindMagicIndex(new int[] { 1, 2, 3, 3, 4, 5, 7, 7, 7, 7 })}");
+        }
+
+        static int? FindMagicIndex(int[] arr)
+        {
+            int indexCounter = 0;
+            foreach (int el in arr) {
+                if (el == indexCounter)
+                {
+                    return el;
+                }
+                // Look at the new index.
+                indexCounter += 1;
+            }
+            return null;
+        }
+    }
+}
